a40de3826dc7c602a65a0810bb0bfc680598af91
Antony Lao <antony.lao@laposte.net>
2024-04-29 12:45:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projet_alpha_mobile',
+  webDir: 'dist'
+};
+
+export default config;
